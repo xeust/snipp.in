215176bf0a33f25da3170f3f18706c1c9421abe7
Max <maxeust@gmail.com>
2021-01-27 17:56:43 +0100

    sync to Deta

---
+++ src/components/Syncing.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="syncer">
+    <div v-if="syncing" class="yellow">Syncing with Deta</div>
+    <div v-else class="green">Synced with Deta</div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  computed: {
+    syncing: function () {
+      return this.$store.state.isSyncing;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.syncer {
+  display: flex;
+  align-items: center;
+  height: 30px;
+  padding-left: 16px;
+  font-family: Menlo, Monaco, "Courier New", monospace;
+}
+
+.yellow {
+  color: #eecb30;
+}
+
+.green {
+  color: #27da6b;
+}
+</style>
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,19 @@
 import Dexie from "dexie";
+import { Deta } from 'deta';
 
-const db = new Dexie("hspace");
-db.version(2).stores({
+const files = Deta(process.env.VUE_APP_DETA_KEY).Base("files");
+const openFiles = Deta(process.env.VUE_APP_DETA_KEY).Base("openFiles");
+const activeFiles = Deta(process.env.VUE_APP_DETA_KEY).Base("activeFiles");
+
+const db = {
+  files: files,
+  openFiles: openFiles,
+  activeFiles: activeFiles
+}
+
+
+const db2 = new Dexie("hspace");
+db2.version(2).stores({
   files: "id, parent, name, type, contents, created_at",
   openFiles: "id, editor",
   activeFiles: "editor, id",
